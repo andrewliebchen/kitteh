a1fe282e1ec628e3669728031afce1494a8347a3
Andrew Liebchen <liebchen@fb.com>
2020-06-21 20:59:06 -0700

    Add animal list to foster page

--- imports/ui/Foster.js
+++ imports/ui/Foster.js
@@ -2,23 +2,30 @@ import React from "react";
 import { Box, Heading, Flex } from "theme-ui";
 import { withTracker } from "meteor/react-meteor-data";
 import { Fosters } from "../api/fosters";
+import { Animals } from "../api/animals";
 import { Link } from "react-router-dom";
 
 const Foster = props => (
   <Box>
     {typeof props.foster !== "undefined" && (
-      <Flex>
-        <Link to={`/spaces/${props.match.params.spaceId}`}>Back</Link>
-        <Heading>{props.foster.name}</Heading>
-      </Flex>
+      <Box>
+        <Flex>
+          <Link to={`/spaces/${props.match.params.spaceId}`}>Back</Link>
+          <Heading>{props.foster.name}</Heading>
+        </Flex>
+        {props.animals.map(animal => (
+          <Box key={animal._id}>{animal.name}</Box>
+        ))}
+      </Box>
     )}
   </Box>
 );
 
 export default withTracker(props => {
-  let spaceId = props ? props.match.params.fosterId : "";
+  let fosterId = props ? props.match.params.fosterId : "";
 
   return {
-    foster: Fosters.findOne(spaceId)
+    animals: Animals.find({ fosterId: fosterId }).fetch(),
+    foster: Fosters.findOne(fosterId)
   };
 })(Foster);
