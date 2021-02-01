0ac8fddb95388ce2ebb44bddd6f0fba0885785f9
Andrew Liebchen <liebchen@fb.com>
2021-01-31 16:00:06 -0800

    Data table for animal

--- src/Animal.js
+++ src/Animal.js
@@ -10,14 +10,26 @@ const Animal = props => {
     Object.keys(animal).length > 0 && (
       <div>
         <h1>{animal.fields.Name}</h1>
-        {weights.map(weight => (
-          <div key={weight.id}>
-            {weight.fields.Weight}
-            <small>
-              {dayjs(weight.fields.Created).format("dddd MMM M [at] h:mm a")}
-            </small>
-          </div>
-        ))}
+        <table>
+          <thead>
+            <tr>
+              <th>Weight</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {weights.map(weight => (
+              <tr key={weight.id}>
+                <td>{weight.fields.Weight}</td>
+                <td>
+                  {dayjs(weight.fields.Created).format(
+                    "dddd MMM M [at] h:mm a"
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     )
   );
