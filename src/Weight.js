085981f258787b18598da073edff7c90c15800b3
Andrew Liebchen <liebchen@fb.com>
2021-02-03 10:04:50 -0800

    Weight rounding and precisions

--- src/Weight.js
+++ src/Weight.js
@@ -1,19 +1,22 @@
 import { units } from "./helpers";
 import { useContext } from "react";
 import AppContext from "./AppContext";
+import Decimal from "decimal.js-light";
+import { Text } from "theme-ui";
 
 const Weight = props => {
   const { unit } = useContext(AppContext);
 
   const unitMeta = units[unit];
-  const value =
-    unit === "grams" ? props.value : props.value * unitMeta.conversion;
+  const value = new Decimal(
+    unit === "grams" ? props.value : props.value * unitMeta.conversion
+  );
 
   return (
-    <b>
-      {value}
+    <Text sx={{ fontWeight: "bold", fontFamily: "monospace" }}>
+      {value.toFixed(2).toString()}
       {unitMeta.short}
-    </b>
+    </Text>
   );
 };
 
